243bdf2bda9a400e867058549804834ee41d7753
Anel Guel <anelguel@gmail.com>
2022-11-07 22:45:56 -0500

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config({ path: ".env" });
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const Private_Key = "417a1a6523e99b000a29a26806c4cdf16d17cf8cbf6f6b0bfee34cff711da577"
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
   	goerli: {
-  		url: `https://holy-winter-ensemble.ethereum-goerli.discover.quiknode.pro/3ed4afb6a88b737572c3e61fafcda560e177aaeb/`,
-  		accounts: [`0x${Private_Key}`]
+  		url: QUICKNODE_HTTP_URL,
+  		accounts: [`0x${PRIVATE_KEY}`]
   	}
   }
 };
